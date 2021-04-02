84ef1445c934cd0d816afabf0529d8efb43c2921
ph4z <pierrick@poseidon-networks.com>
2021-04-02 15:36:05 +0000

    (feature) Initial support for live exec

--- src/components/LiveStrategyExecutor/LiveStrategyExecutor.container.js
+++ src/components/LiveStrategyExecutor/LiveStrategyExecutor.container.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux'
 
+import WSActions from '../../redux/actions/ws'
 import LiveStrategyExecutor from './LiveStrategyExecutor'
 import { getMarkets } from '../../redux/selectors/meta'
 
@@ -8,8 +9,12 @@ const mapStateToProps = (state = {}) => ({
 })
 
 const mapDispatchToProps = () => ({
-  dsExecuteLiveStrategy: () => {
+  dsExecuteLiveStrategy: (exchange, from, to, symbol, tf, strategy) => {
+  // dsExecuteLiveStrategy: () => {
     // TODO execute
+    dispatch(WSActions.send([
+      'exec.live', [exchange, from, to, symbol, tf, true, true, true, strategy],
+    ]))
   },
 })
 
