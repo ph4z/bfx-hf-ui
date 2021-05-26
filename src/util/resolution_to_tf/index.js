9f3e8c673dfa1da296bb698d21433056c08feede
Charles <charlau30@gmail.com>
2021-05-26 14:31:24 +0200

    (feature): exchanges tf

--- src/util/resolution_to_tf/index.js
+++ src/util/resolution_to_tf/index.js
@@ -1,11 +1,15 @@
 import bitfinex from './bitfinex'
 import binance from './binance'
 import binance_futures from './binance_futures'
-import binance from './kraken'
+import binance_coins from './binance_coins'
+import kraken from './kraken'
+import ftx from './ftx'
 
 export default {
   bitfinex,
   binance,
   binance_futures,
+  binance_coins,
   kraken,
+  ftx,
 }
