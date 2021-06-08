ff0f110fdce855f7606d0ffd2c141ac6849f36ea
Charles <charlau30@gmail.com>
2021-06-08 19:18:42 +0200

    (fix) render layout

--- src/transfers/transfer.js
+++ src/transfers/transfer.js
@@ -18,7 +18,7 @@ export default () => ({
     name: 'general',
     rows: [
       ['amount', 'symbol'],
-      ['exchangeOrig', 'exchangeDest'],
+      ['walletOrig', 'walletDest'],
     ],
   }],
 
